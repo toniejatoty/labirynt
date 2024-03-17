7f5054f3d0280c07beebb53d7c871fa3a64318e3
Aary <piotrsosnowski@autograf.pl>
2024-03-17 20:47:29 +0100

    Poprawiłem strukturę i writer Zrobiłem test i działało dobrze

--- lab_struct.h
+++ lab_struct.h
@@ -3,10 +3,10 @@
 
 typedef struct elem {
 	int nr;			//nr wierzchołka, -2 = STOP
-	int kierunek;		//zmiana kierunku w porównaniu do poprzedniego wierzchołka(pierwszego elementu listy): -1 - turn left, 1 - turn right
+	int kierunek;		//zmiana kierunku w porównaniu do poprzedniego wierzchołka(pierwszego elementu listy): -1 - turn left, 1 - turn right, 0 - ten sam kierunek
 	int odl;		//odległość między wierzchołkami
 	int czy_min;		//czy następny na najkrótszej ścieżce: 1 - tak, 0 - nie
-	elem *next;
+	struct elem *next;
 } *list_t;
 
 #endif
--- writer.c
+++ writer.c
@@ -1,32 +1,36 @@
 #include "writer.h"
 
 void save(int i, FILE *out, list_t *T) {
-	fprintf(out, "START\n");
-	list_t pointer = T[i];
+	if (i == 0)
+		fprintf(out, "START\n");
+	if(T[i] == NULL)
+		exit(EXIT_FAILURE);
+	list_t pointer = malloc(sizeof*pointer);
+	pointer->next = T[i];
+	pointer=pointer->next;
 	while(pointer->czy_min != 1) {
-		if(pointer->next == NULL)
-			exit EXIT_FAILURE;
+		if(pointer == NULL)
+			exit(EXIT_FAILURE);
 		pointer=pointer->next;
 	}
-	if (i == 0)
-		fprintf(out, "FORWARD %d\n", pointer->odl);
-	else {
-		switch(pointer->kierunek) {
-			case -1:
-				fprintf(out,"TURNLEFT\n");
-				break;
-			case 1:
-				fprintf(out,"TURNRIGHT\n");
-				break;
-			default:
-				fprintf(stderr, "Nie powinienem tu być!\n");
-				break;	
-		}
-		fprintf(out, "FORWARD %d\n", pointer->odl);
+	switch(pointer->kierunek) {
+		case -1:
+			fprintf(out,"TURNLEFT\n");
+			break;
+		case 1:
+			fprintf(out,"TURNRIGHT\n");
+			break;
+		case 0:
+			break;
+		default:
+			fprintf(stderr, "Nie powinienem tu być!\n");
+			break;	
 	}
+	fprintf(out, "FORWARD %d\n", pointer->odl);
+	
 	if(pointer->nr == -2) {
 		fprintf(out, "STOP\n");
-		exit EXIT_SUCCESS;
+		exit(EXIT_SUCCESS);
 	}
-	save(pointer->nr, out, T);
+	save(pointer->nr-1, out, T);
 }
