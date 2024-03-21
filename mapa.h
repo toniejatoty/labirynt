87d09e230f87593df9700cdee74ae6329b3d0c96
Konrad <konrad.wolkiewicz@o2.pl>
2024-03-21 21:33:28 +0100

    usunalem mapa.c i wprowadzilem funkcje z niego do solvera i switcha

--- mapa.h
+++
@@ -1,6 +0,0 @@
-#ifndef _MAPA_H_
-#define _MAPA_H_
-char ** whereP(char ** maze, int l , int c, char * input, int *T);
-char **load(char **maze, char * input, int *T);
-
-#endif
--- solver.c
+++ solver.c
@@ -1,4 +1,3 @@
-#include "mapa.h"
 #include "solver.h"
 #include "switch.h"
 #include <limits.h>
@@ -7,6 +6,115 @@
 #include <string.h>
 #include "structure.h"
 #include "zwalnianie.h"
+char **whereP(char ** maze, int l , int c, char * input, int *T)
+{
+        maze = load(maze, input, T);
+        char a='K';
+        while(a!='P')
+        {
+if(T[2]==0&&T[3]==0)
+{
+                        for(int i=0; i<T[6]; i++)
+                        {
+                        if(maze[0][i]=='P'){a='P';T[7]=0; T[8]=i; }
+                        }
+                for(int j=0; j<T[4]; j++)
+                {
+        if(maze[j][0]=='P') // TU MOZE BYC BLAD bo nie koniecznie jest to spojne
+        {
+        a='P';
+        T[7]=j;
+        T[8]=0;
+        }
+        if(maze[j][T[6]]=='P')
+        {
+        a='P';
+        T[7]=j;
+        T[8]=T[6];
+        }
+
+                }for(int i=0; i<T[6]; i++)
+                        {
+                        if(maze[T[4]][i]=='P'){a='P';T[7]=T[4]; T[8]=i;}
+                        }
+}
+
+        else if(T[2]==0){
+                        for(int i=0; i<T[6]; i++)
+                        {
+                        if(maze[0][i]=='P'){a='P';T[7]=0; T[8]=i; }
+                        }
+                for(int j=0; j<100; j++)
+                {
+        if(maze[j][0]=='P')
+        {
+        a='P';
+        T[7]=j;
+        T[8]=0;
+        }
+        if(maze[j][T[6]]=='P')
+        {
+        a='P';
+        T[7]=j;
+        T[8]=T[6];
+        }
+
+                }
+                }
+                else if(T[2]==T[3]){
+                        for(int i=0; i<T[6]; i++)
+                        {
+                        if(maze[T[4]][i]=='P'){a='P';T[7]=T[4]; T[8]=i;}
+                        }
+        for(int j=0; j<T[4]; j++)
+                {
+        if(maze[j][0]=='P')
+        {
+        a='P';
+        T[7]=j;
+        T[8]=0;
+        }
+        if(maze[j][T[6]]=='P')
+        {
+        a='P';
+        T[7]=j;
+        T[8]=T[6];
+        }
+
+
+                }}
+        else if(T[2]!=T[3])
+        {
+        for(int j=0; j<100; j++)
+        {
+        if(maze[j][0]=='P')
+        {
+        a='P';
+        T[7]=j;
+        T[8]=0;
+        }
+        if(maze[j][T[6]]=='P')
+        {
+        a='P';
+        T[7]=j;
+        T[8]=T[6];
+        }
+        }
+
+
+        }
+        if(a!='P')
+        {
+                if(T[2]==T[3])T[2]=0;
+                else T[2]++;
+        maze=load(maze , input,T);
+
+        }
+        }
+T[9]=T[2];
+        return maze;
+}
+
 char wasvisited(structure start, structure position, structure special)
 { 
         char rreturn;
