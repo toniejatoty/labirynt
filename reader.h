08afafe72223ff6b79ca6c056f1cecff2d157364
Aary <piotrsosnowski@autograf.pl>
2024-03-25 22:38:31 +0100

    Poprawiłem reader.h

--- reader.h
+++ reader.h
@@ -2,7 +2,6 @@
 #define _READER_H_
 
 char czypoprawntlab(char *input, char * aout);
-char czypoprawnylab(char *input, char *aout);
 char bledy(char * input,char a, int ktorywiersz, int ktorakolumna, int wspolrzednaPx, int wspolrzednaPy, int wspolrzednaKx, int wspolrzednaKy, char komunikat, int T[] );
 int *rozmiar(char * input);
 
